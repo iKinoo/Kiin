ed6847716e379dc9e2af24cd21185e255589c138
Gerardo Villanueva <a22216369@alumnos.uady.mx>
2024-12-16 23:48:57 -0600

    feat: add particle background to hero sectionr

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from 'next/link'
 import React from 'react'
+import { useState } from 'react'
 interface NavBarProps {
     links: { label: string, route: string }[]
 }
@@ -23,11 +24,7 @@ const AppLogo = () => {
     return (
         <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <div className="flex items-center justify-center h-8 w-8 logo">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 dark:text-white">
-                    <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
-                    <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
-                    <path d="M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z" />
-                </svg>
+                <img src="/img/logo/color_white.png" alt="" />
             </div>
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Kiin</span>
         </Link>
@@ -35,25 +32,63 @@ const AppLogo = () => {
 }
 
 const NavBar: React.FC<NavBarProps> = ({ links }) => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
 
-        <nav className='bg-white border-gray-200 dark:bg-gray-900'>
-            <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        <nav className="bg-white border-gray-200 dark:bg-gray-900" >
+            <div className="container flex flex-wrap items-center justify-between mx-auto py-2">
                 <AppLogo />
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className='navbar-links'>
-                        {
-                            links.map((link, index) => (
-                                navBarItem(link, index)
-                            ))
-                        }
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700"
+                    aria-controls="navbar-default"
+                    aria-expanded="false"
+                >
+                    <span className="sr-only">Abrir menú</span>
+                    {isOpen ? (
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                            ></path>
+                        </svg>
+                    ) : (
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h16M4 18h16"
+                            ></path>
+                        </svg>
+                    )}
+                </button>
+                <div
+                    className={`${isOpen ? "block" : "hidden"
+                        } w-full md:block md:w-auto`}
+                >
+                    <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        {links.map((link, index) => navBarItem(link, index))}
                     </ul>
                 </div>
             </div>
-
-
         </nav>
-    )
-}
+    );
+};
+
 
 export default NavBar
