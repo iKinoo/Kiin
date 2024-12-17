d2469a4f2221aa1d16f0da2c87dc4110198a13c1
Gerardo Villanueva <a22216369@alumnos.uady.mx>
2024-12-16 23:54:09 -0600

    styles: adjust layout overflow to hero section

--- src/app/HomeContent.tsx
+++ src/app/HomeContent.tsx
@@ -2,29 +2,42 @@
 import React from 'react'
 import NavBar from './components/NavBar';
 import Link from 'next/link';
-
+import ParticlesComponent from './components/Particles';
 const HomeContent: React.FC = () => {
-    const links = [
-      { label: 'Inicio', route: '' },
-    ]
-    return (
-      <div className="bg-white text-black min-h-screen">
-        <NavBar links={links} />
-        <div className="container mx-auto flex items-center justify-center min-h-screen gap-1">
-          <div className="max-w-2xl flex flex-col gap-10 items justify-center">
-            <h1 className="text-7xl leading-tight">Organiza tu carga acádemica de forma fácil y visual</h1>
-            <p className="text-2xl">Una herramienta de y para estudiantes</p>
-            <Link href={'/calendar'}>
-                <button className="block py-2 px-10 bg-orange-500 hover:bg-orange-700 text-white font-bold rounded">Comenzar</button>
-            </Link>
-          </div>
-          <div className="m-1 h-[45vh] w-[45vh] rounded-full rounded-br-none border-2 bg-gray-900 img_container">
-            <img className="mt-7 ml-7 object-cover h-5/6 w-5/6 rounded-full"
-            src="https://images.ctfassets.net/pdf29us7flmy/50kVKgdwULKaOgvkJBRic5/68591241ae297e886978aa9f17d16e00/resized.png?w=720&q=100&fm=jpg" alt="Img bienvenida kiin" />
-          </div>
+  const links = [
+    { label: 'Inicio', route: '' },
+  ]
+  return (
+    <div className='bg-white text-black h-screen flex flex-col overflow-hidden'>
+      <ParticlesComponent/>
+    <div className="z-10 relative">
+      <NavBar links={links}/>
+      <div className="container mx-auto flex flex-1 flex-col-reverse lg:flex-row items-center justify-center min-h-screen gap-6 px-4">
+        <div className="max-w-2xl text-center lg:text-left flex flex-col gap-10 items-center lg:items-start justify-center">
+          <h1 className="text-5xl sm:text-5xl lg:text-7xl leading-tight">
+            Organiza tu carga académica de forma fácil y visual
+          </h1>
+          <p className="text-lg sm:text-xl lg:text-2xl">
+            Una herramienta de y para estudiantes
+          </p>
+          <Link href={'/calendar'}>
+            <button className="block py-3 px-10 bg-orange-500 hover:bg-orange-700 text-white font-bold rounded-xl transform transition-transform duration-150 active:scale-110">
+              Comenzar
+            </button>
+          </Link>
+        </div>
+        <div className="mx-16 flex justify-center lg:justify-start mt-6 lg:mx-0 xl:mx-0 2xl:mx-9">
+          <img
+            className="max-w-full h-auto sm:h-80 sm:w-auto lg:h-5/6 lg:w-auto object-cover"
+            src="img/banner.gif"
+            alt="Img bienvenida kiin"
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+    </div>
+
+  );
+}
 
 export default HomeContent
