3594e501eae7a309f77b260a0423872f269be06c
Rodrigo Pacab <rodri27red@gmail.com>
2024-12-18 12:18:29 -0600

    fix: reading csv from url

--- src/pages/api/data/CoursesModelDAO.ts
+++ src/pages/api/data/CoursesModelDAO.ts
@@ -1,6 +1,6 @@
 import csvParser from "csv-parser";
-import fs from 'fs';
 import { CourseCSV } from "./CourseModel";
+import { Readable } from "stream";
 
 
 export class CoursesModelDao {
@@ -11,20 +11,39 @@ export class CoursesModelDao {
       return this._results;
     }
 
-    const filePath = 'public/data.csv'; // Ruta del archivo
-    this._results = await readCSV(filePath);
+    const url = `${process.env.VERCEL_URL}/data.csv`;
+    // const response = await fetch(`https://${url}`); // Asegúrate de usar https
+    //const filePath = 'public/data.csv'; // Ruta del archivo
+    this._results = await readCSV(url);
     return this._results;
   }
 }
 
-const readCSV = async (filePath: string): Promise<CourseCSV[]> => {
-  return new Promise((resolve, reject) => {
-    const results: CourseCSV[] = [];
+const readCSV = async (url: string): Promise<CourseCSV[]> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(url);
 
-    fs.createReadStream(filePath) // Leer el archivo CSV
-      .pipe(csvParser()) // Pasarlo a través del parser
-      .on('data', (data) => results.push(data)) // Procesar cada fila
-      .on('end', () => resolve(results)) // Resolver al finalizar
-      .on('error', (error) => reject(error)); // Manejar errores
+      if (!response.ok) {
+        return reject(new Error(`Failed to fetch CSV file: ${response.statusText}`));
+      }
+
+      const csvText = await response.text(); // Obtener el contenido del archivo CSV como texto
+
+      const results: CourseCSV[] = [];
+
+
+      // Usar un stream legible para procesar el texto del CSV
+      
+      const readableStream = Readable.from(csvText);
+
+      readableStream
+        .pipe(csvParser())
+        .on('data', (data) => results.push(data)) // Procesar cada fila
+        .on('end', () => resolve(results)) // Resolver al finalizar
+        .on('error', (error) => reject(error)); // Manejar errores
+    } catch (error) {
+      reject(error); // Manejar cualquier error de fetch o de otro tipo
+    }
   });
 };
