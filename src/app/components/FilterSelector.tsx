2fc959c729124c4ebe2cb1f56fc9e6ca9e1775ad
GeraVillla <a22216369@alumnos.uady.mx>
2024-12-27 19:21:01 -0600

    feat(mobile): implements toggle to FilterSelector

--- src/app/components/FilterSelector.tsx
+++ src/app/components/FilterSelector.tsx
@@ -8,13 +8,13 @@ interface FilterSelectorProps {
     categories: Category[]
     onClick: (newCategories: Category[]) => void
     onSubmit: () => void
-    onChanceSliderValue:(value: number | number[]) => void;
+    onChanceSliderValue: (value: number | number[]) => void;
     maxSliderValue: number;
-
+    toggleSideBar: () => void;
 }
 
 
-const FilterSelector: React.FC<FilterSelectorProps> = ({ categories, onClick, onSubmit ,onChanceSliderValue, maxSliderValue}) => {
+const FilterSelector: React.FC<FilterSelectorProps> = ({ categories, onClick, onSubmit, onChanceSliderValue, maxSliderValue, toggleSideBar }) => {
     const refreshCategories = (categoryIndex: number, valueId: number) => {
         const newCategories = [...categories];
         const category = newCategories[categoryIndex]
@@ -22,27 +22,24 @@ const FilterSelector: React.FC<FilterSelectorProps> = ({ categories, onClick, on
         newCategories[categoryIndex] = category;
         categories.forEach((cat) => cat.filterWithCategories(newCategories));
         onClick(newCategories);
-    }   
+    }
 
     return (
         <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
             <ul className="space-y-2 font-medium">
-                <li className='flex items-center p-2 text-gray-900 rounded-lg dark:text-white group'>
-                <span className="ms-3">Filtrar por</span>
-                </li>
 
                 {
                     categories.map((category, index) => (
                         <CategorySelector key={index} category={category} onClick={(valueId) => refreshCategories(index, valueId)} />
                     ))
-                    
+
                 }
                 <li>
-                <SliderFilter maxValue={maxSliderValue} label='Materias por horario' objectNameCounting='materias' onValueChange={onChanceSliderValue} />
+                    <SliderFilter maxValue={maxSliderValue} label='Materias por horario' objectNameCounting='materias' onValueChange={onChanceSliderValue} />
                 </li>
-                
+
                 <li>
-                    <button onClick={onSubmit} className="justify-self-center block mt-12 py-2 px-10 bg-purple-500 hover:bg-purple-800 text-white font-bold rounded">
+                    <button onClick={() => { onSubmit(); toggleSideBar(); }} className="justify-self-center block mt-12 py-2 px-10 bg-purple-500 hover:bg-purple-800 text-white font-bold rounded">
                         Aplicar
                     </button>
                 </li>
