14a450d6994afafc59dcf25552b92df89cc51c90
GeraVillla <a22216369@alumnos.uady.mx>
2024-12-29 15:59:50 -0600

    styles(mobile): add dark mode palette to bar components

--- src/app/components/FilterSelector.tsx
+++ src/app/components/FilterSelector.tsx
@@ -25,26 +25,26 @@ const FilterSelector: React.FC<FilterSelectorProps> = ({ categories, onClick, on
     }
 
     return (
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-            <ul className="space-y-2 font-medium">
+        <>
+            <div className="h-4/6 overflow-y-auto">
+                <ul className="space-y-2 font-medium">
 
-                {
-                    categories.map((category, index) => (
-                        <CategorySelector key={index} category={category} onClick={(valueId) => refreshCategories(index, valueId)} />
-                    ))
+                    {
+                        categories.map((category, index) => (
+                            <CategorySelector key={index} category={category} onClick={(valueId) => refreshCategories(index, valueId)} />
+                        ))
 
-                }
-                <li>
-                    <SliderFilter maxValue={maxSliderValue} label='Materias por horario' objectNameCounting='materias' onValueChange={onChanceSliderValue} />
-                </li>
+                    }
+                    <li>
+                        <SliderFilter maxValue={maxSliderValue} label='Materias por horario' objectNameCounting='materias' onValueChange={onChanceSliderValue} />
+                    </li>
 
-                <li>
-                    <button onClick={() => { onSubmit(); toggleSideBar(); }} className="justify-self-center block mt-12 py-2 px-10 bg-purple-500 hover:bg-purple-800 text-white font-bold rounded">
-                        Aplicar
-                    </button>
-                </li>
-            </ul>
-        </div>
+                </ul>
+            </div>
+            <button onClick={() => { onSubmit(); toggleSideBar(); }} className="justify-self-center block mt-12 py-2 px-10 bg-purple-500 hover:bg-purple-800 text-white font-bold rounded">
+                Aplicar
+            </button>
+        </>
     )
 }
 
