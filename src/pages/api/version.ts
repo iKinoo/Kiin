f3af5fe98008066c5d076bb7b20c04648457f2c6
Rodrigo Pacab <rodri27red@gmail.com>
2025-01-02 15:59:25 -0600

    chore: update schedules data for 02.01.2025

--- src/pages/api/version.ts
+++ src/pages/api/version.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-const currentVersion = "1.1_19.12.2024"
+const currentVersion = "1.1_02.01.2025"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
