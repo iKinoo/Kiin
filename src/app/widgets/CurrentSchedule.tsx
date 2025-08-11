93bf133819106a0ea926677d3142d18929c849e6
RodrigoPacab <rodri27red@gmail.com>
2025-08-11 15:48:13 -0600

    ui: update Google sign-in prompt and improve button styles

--- src/app/widgets/CurrentSchedule.tsx
+++ src/app/widgets/CurrentSchedule.tsx
@@ -1,7 +1,6 @@
 import { Course } from '@/domain/entities/Course';
 import { Schedule } from '@/domain/entities/Schedule';
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react';
-import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
 import Pivot from '../../domain/entities/Pivot';
 import GoogleCalendarButton from '../components/GoogleCalendarButton';
@@ -122,15 +121,19 @@ function CurrentSchedule({ schedule, pivots, label, pinnedSubjects }: Props) {
                             const Swal = (await import('sweetalert2')).default;
                             await Swal.fire({
                                 icon: 'info',
-                                title: 'Acceso requerido',
-                                text: 'Debes iniciar sesión con Google para exportar tu horario.',
+                                title: 'Acceso Requerido',
+                                text: 'Debes iniciar sesión con Google para exportar tu horario. Nos encontramos en proceso de validación de Google pero tranquil@, no mordemos.',
+
                                 confirmButtonText: 'Iniciar sesión'
+                                
                             });
                             await GoogleSignIn();
                         }}
-                        className="hidden bg-blue-600  p-2 rounded-full items-center justify-center"
+                        className=" bg-blue-700  p-2 rounded-full items-center justify-center"
                     >
-                        <Image src={'/img/google_calendar_mono.svg'} alt='google' width={24} height={24} />
+                        <svg className='-m-1' xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="34" height="34" viewBox="0 0 48 48">
+                            <rect width="22" height="22" x="13" y="13" fill="#fff"></rect><polygon fill="#1e88e5" points="25.68,20.92 26.688,22.36 28.272,21.208 28.272,29.56 30,29.56 30,18.616 28.56,18.616"></polygon><path fill="#1e88e5" d="M22.943,23.745c0.625-0.574,1.013-1.37,1.013-2.249c0-1.747-1.533-3.168-3.417-3.168 c-1.602,0-2.972,1.009-3.33,2.453l1.657,0.421c0.165-0.664,0.868-1.146,1.673-1.146c0.942,0,1.709,0.646,1.709,1.44 c0,0.794-0.767,1.44-1.709,1.44h-0.997v1.728h0.997c1.081,0,1.993,0.751,1.993,1.64c0,0.904-0.866,1.64-1.931,1.64 c-0.962,0-1.784-0.61-1.914-1.418L17,26.802c0.262,1.636,1.81,2.87,3.6,2.87c2.007,0,3.64-1.511,3.64-3.368 C24.24,25.281,23.736,24.363,22.943,23.745z"></path><polygon fill="#fbc02d" points="34,42 14,42 13,38 14,34 34,34 35,38"></polygon><polygon fill="#4caf50" points="38,35 42,34 42,14 38,13 34,14 34,34"></polygon><path fill="#1e88e5" d="M34,14l1-4l-1-4H9C7.343,6,6,7.343,6,9v25l4,1l4-1V14H34z"></path><polygon fill="#e53935" points="34,34 34,42 42,34"></polygon><path fill="#1565c0" d="M39,6h-5v8h8V9C42,7.343,40.657,6,39,6z"></path><path fill="#1565c0" d="M9,42h5v-8H6v5C6,40.657,7.343,42,9,42z"></path>
+                        </svg>
 
 
 
@@ -235,7 +238,7 @@ function ShareLinkButton({ schedule, setShowShareLink, showShareLink }: ShareLin
                     const shareLink = `${window.location.href}/horario?ids=${coursesIds.toString()}`;
                     setShowShareLink(shareLink);
                 }}
-                className="bg-blue-600  p-2 rounded-full flex items-center justify-center"
+                className="bg-blue-700  p-2 rounded-full flex items-center justify-center"
             >
                 <svg className='' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M680-80q-50 0-85-35t-35-85q0-6 3-28L282-392q-16 15-37 23.5t-45 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q24 0 45 8.5t37 23.5l281-164q-2-7-2.5-13.5T560-760q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-24 0-45-8.5T598-672L317-508q2 7 2.5 13.5t.5 14.5q0 8-.5 14.5T317-452l281 164q16-15 37-23.5t45-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-80q17 0 28.5-11.5T720-200q0-17-11.5-28.5T680-240q-17 0-28.5 11.5T640-200q0 17 11.5 28.5T680-160ZM200-440q17 0 28.5-11.5T240-480q0-17-11.5-28.5T200-520q-17 0-28.5 11.5T160-480q0 17 11.5 28.5T200-440Zm480-280q17 0 28.5-11.5T720-760q0-17-11.5-28.5T680-800q-17 0-28.5 11.5T640-760q0 17 11.5 28.5T680-720Zm0 520ZM200-480Zm480-280Z" /></svg>
 
