fe5f46cb854c086b0bfda9d2d024f5bf6b766bba
RodrigoPacab <rodri27red@gmail.com>
2025-12-10 12:45:45 -0600

    fix: update SubjectCard to show professors by default and enhance layout for credits and degree display

--- src/app/widgets/SubjectsView.tsx
+++ src/app/widgets/SubjectsView.tsx
@@ -63,7 +63,7 @@ function SubjectsView({
                     className="border-2 border-gray-500 bg-gray-800 text-gray-100 flex flex-row gap-2 rounded-md items-center p-2"
                     type="button"
                 >
-                    Materias
+                    Selecciona tus Materias
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
@@ -138,7 +138,7 @@ const colors = [
 
 function SubjectCard({ subject, allProfessors, pivots, setPivots, pinnedSubjects, setPinnedSubjects }: SubjectCardProps) {
 
-    const [showProfessors, setShowProfessors] = useState(false);
+    const [showProfessors, setShowProfessors] = useState(true);
 
     const isSelected = pinnedSubjects.find(subj => subj === subject.id) !== undefined
 
@@ -185,31 +185,38 @@ function SubjectCard({ subject, allProfessors, pivots, setPivots, pinnedSubjects
             {subject.type}
             {/* <div className='h-2 w-2 rounded-large dark:bg-gray-400 bg-gray-400 inline-block'></div>
             8 Créditos */}
+            <div className='h-2 w-2 bg-dark dark:bg-white rounded-full '></div>
+            {subject.credits} Créditos
         </div>
         <div className='dark:text-gray-400 text-gray-600 mb-1'>
             Semestre {(Array.from((new Set(subject.semestre)))).join(", ")}
         </div>
-        <div className='flex flex-row items-center mb-2 dark:text-gray-400 text-gray-600 m'>
-            <svg className='mr-2 dark:fill-white fill-black' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-120 200-272v-240L40-600l440-240 440 240v320h-80v-276l-80 44v240L480-120Zm0-332 274-148-274-148-274 148 274 148Zm0 241 200-108v-151L480-360 280-470v151l200 108Zm0-241Zm0 90Zm0 0Z" /></svg>
-            {subject.degreeResume.split("-").join(", ")}
+        <div className='flex justify-between flex-row items-center mb-2 dark:text-gray-400 text-gray-600 m'>
+            <div className='flex flex-row items-center gap-1'>
+                <svg className='mr-2 dark:fill-white fill-black' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-120 200-272v-240L40-600l440-240 440 240v320h-80v-276l-80 44v240L480-120Zm0-332 274-148-274-148-274 148 274 148Zm0 241 200-108v-151L480-360 280-470v151l200 108Zm0-241Zm0 90Zm0 0Z" /></svg>
+                {subject.degreeResume.split("-").join(", ")}
+            </div>
+            
+            
+            <div className='flex flex-row justify-end'>
+                <button
+                    onClick={
+                        () => setShowProfessors(!showProfessors)
+                    }
+                    className={`text-white flex flex-row items-center border border-gray-500 rounded-lg w-max p-2 text-sm ${showProfessors ? "bg-gray-700" : "bg-gray-800"}`}>
+                    {showProfessors ?
+                        <svg className='inline mr-1' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z" /></svg>
+                        : <svg className='inline mr-1' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z" /></svg>
+                    }
+                    <span className='' style={{ lineHeight: 0 }}>
+                        Profesores
+                    </span>
+                </button>
+            </div>
         </div>
 
 
-        <div className='flex flex-row justify-end'>
-            <button
-                onClick={
-                    () => setShowProfessors(!showProfessors)
-                }
-                className={`text-white flex flex-row items-center border border-gray-500 rounded-lg w-max p-2 text-sm ${showProfessors ? "bg-gray-700" : "bg-gray-800"}`}>
-                {showProfessors ?
-                    <svg className='inline mr-1' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z" /></svg>
-                    : <svg className='inline mr-1' xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Zm0-300Zm0 220q113 0 207.5-59.5T832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280Z" /></svg>
-                }
-                <span className='' style={{ lineHeight: 0 }}>
-                    Profesores
-                </span>
-            </button>
-        </div>
+
         <div className={`flex flex-col gap-1 ${showProfessors ? "static" : "hidden"}`}>
             {allProfessors.filter(professor => subject.professors.includes(professor.id)).map(e => (
                 <ProfessorRow setPivots={setPivots} pivots={pivots} key={e.id} professor={e} idSubject={subject.id}></ProfessorRow>
